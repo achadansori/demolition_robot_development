bd6e11f1cd3f57c96776e9a1e3ea6160ea6e5f22
Muhammad Achad Ansori <94736530+achadansori@users.noreply.github.com>
2025-12-17 08:34:41 +0700

    gpio

--- control_demoliiton_robot/Core/Src/main.c
+++ control_demoliiton_robot/Core/Src/main.c
@@ -10,6 +10,7 @@
 #include "main.h"
 #include "i2s.h"
 #include "spi.h"
+#include "tim.h"
 #include "usart.h"
 #include "usb_device.h"
 #include "gpio.h"
@@ -102,6 +103,11 @@ int main(void)
   MX_SPI1_Init();
   MX_USART1_UART_Init();
   MX_USB_DEVICE_Init();
+  MX_TIM1_Init();
+  MX_TIM2_Init();
+  MX_TIM3_Init();
+  MX_TIM4_Init();
+  MX_TIM8_Init();
   /* USER CODE BEGIN 2 */
 
   // ========================================================================
