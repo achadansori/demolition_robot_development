403e822e178db8a7fcef9b8b8792cd52f136e518
Muhammad Achad Ansori <94736530+achadansori@users.noreply.github.com>
2026-05-05 08:58:53 +0700

    Untrack files matching .gitignore and remove leftover backups
    Removes 710 files from index (Debug/, .metadata/, build/, install/, log/,
    .settings/, .cproject, .project, *.launch, *.log, *.bak, *.backup).
    Files remain on disk except backup files which are deleted.

--- build/brokk_control/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/achadansori/STM32CubeIDE/demolition_robot_development/install/brokk_control'
--- build/brokk_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/achadansori/STM32CubeIDE/demolition_robot_development/brokk_demolition_sim/src/brokk_description
-# Build directory: /home/achadansori/STM32CubeIDE/demolition_robot_development/build/brokk_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
